2e30108ff51985b9acb7a07221be665fbba69e27
José Ricardo da Silva Ferreira <jrsf68@gmail.com>
2021-05-17 08:56:32 -0300

    aula do dia 17-05-2021
    Programas desenvolvidos na aula de Programação de Software Básico.

--- bit_operacao_e.c
+++ bit_operacao_e.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main(void)
 {
+    // Declaracao das variáveis
     int A = 4, B = 5, C, D;
-    C = ~A;
+    // "E Lógico" no nível de bit.
+    C = A & B;
+    // "E Lógico" no nível de byte (ou no nível do dado).
     D = A && B;
+    // Resultados.
     printf("C = %d\n", C);
     printf("D = %d\n", D);
     return 0;
