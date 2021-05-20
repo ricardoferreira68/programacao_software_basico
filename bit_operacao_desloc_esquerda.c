f2e8a162d95fc0dc04e56db32538fb521377b20b
José Ricardo da Silva Ferreira <jrsf68@gmail.com>
2021-05-20 08:26:05 -0300

    Aula 19-05-2021
    Programas da aula

--- bit_operacao_desloc_esquerda.c
+++ bit_operacao_desloc_esquerda.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main(void)
 {
-    char A = 1, B, C, D, E;
+    short A = 0b0001000000000000, B, C, D, E;
     B = A << 1;
     C = A << 2;
     D = A << 3;
     E = A << 4;
-    printf("short = %d\n\n", sizeof(char));
-    printf("A = %d\n\n", A);
-    printf("B = %d\n\n", B);
-    printf("C = %d\n\n", C);
-    printf("C = %d\n\n", D);
-    printf("C = %d\n\n", E);
+    printf("short = %d\n\n", sizeof(short));
+    printf("A = %d\n\n", A);   //  0001000000000000
+    printf("B = %d\n\n", B);   //  0010000000000000
+    printf("C = %d\n\n", C);   //  0100000000000000
+    printf("D = %d\n\n", D);   //  1000000000000000
+    printf("E = %d\n\n", E);   //1 0000000000000000
     return 0;
 }
